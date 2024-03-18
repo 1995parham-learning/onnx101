f39d66f47c8eb198154b746821770bb443bf7a86
Parham Alvani <parham.alvani@gmail.com>
2024-03-18 07:58:47 +0000

    feat: define a new component for notes

---
+++ components/Note.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="p-2 border dark:bg-yellow:20 bg-yellow dark:border-gray-700 rounded">
+    <p class="dark:color-orange">
+      <heroicons-exclamation-circle-solid />
+      Note
+    </p>
+    <slot />
+  </div>
+</template>
