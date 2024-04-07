dd37e98e2cbfbb96c6e68d450ba6cb61ce473939
Parham Alvani <parham.alvani@gmail.com>
2024-04-07 16:51:34 +0000

    feat: update download script

--- src/model_dlw.sh
+++ src/model_dlw.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-mcli cp smapp-teh1/nostra-baly/1/rf_1_25_85_0_0_0_0.pkl model.pkl
+# download the random forest (for baly) model from s3
+# mc cp smapp-teh1/nostra-baly/1/rf_1_25_85_0_0_0_0.pkl model.pkl
+
+# download the neural network (for gilan) model from s3
+mkdir nn-model
+mc cp -r smapp-teh1/nostradamus/gilan_16_13_10_7_haversine_day_type_recent_tf/0001/ nn-model
