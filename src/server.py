af81e97b8787c5626e144c4407a45722640168b1
Parham Alvani <parham.alvani@gmail.com>
2024-04-07 16:42:50 +0000

    feat: serve oonx model using onnx runtime

--- src/server.py
+++ src/server.py
@@ -1,8 +1,29 @@
+"""
+Load the model using ONNX runtime.
+"""
+
 import onnxruntime as ort
 
-# Load the model and create InferenceSession
-model_path = "./model.onnx"
-session = ort.InferenceSession(model_path, providers=["CPUExecutionProvider"])
+if __name__ == "__main__":
+    # Load the model and create InferenceSession
+    MODEL_PATH = "./model.onnx"
+    session = ort.InferenceSession(MODEL_PATH, providers=["CPUExecutionProvider"])
 
-outputs = session.run(output_names={}, input_feed={})
-print(outputs)
+    # Call Nostradamus model
+    outputs = session.run(
+        output_names={},
+        input_feed={
+            "haversine_distance_layer_input": [
+                [
+                    32.058145,
+                    44.326834,
+                    32.028416,
+                    44.333983,
+                    10.500000,
+                    3.373689638972256,
+                    1,
+                ]
+            ]
+        },
+    )
+    print(outputs)
